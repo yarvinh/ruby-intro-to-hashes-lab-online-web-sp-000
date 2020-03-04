d917f764f46a979a80f0015bc21b62fa0b2d12ab
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-04 02:19:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-	monopoly = {:value => {:value_1 => 1,:value_2 => 2, :value_3 => 3},:key => 1,}
+	monopoly = {:value => {:value_1 => 1,:value_2 => 2, :value_3 => 3}}
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 
 end
