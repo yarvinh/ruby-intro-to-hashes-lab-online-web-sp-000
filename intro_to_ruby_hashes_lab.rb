c92aa90fab816a6e82d208db4de7c455a9e6771e
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-04 01:12:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,11 +9,13 @@ end
 
 def monopoly
 	monopoly = {:railroads => {}}
+	
 
 
 end
 
 def monopoly_with_second_tier
+	 monopoly = {:number => {:number_2 => 4}}
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 
 end
